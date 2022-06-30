e8e39e74c8fcdf57cbf271bcf685a3c34596dc8d
Hasan Tugay Birihan <hasantugay.birihan@nttdata.com>
2022-06-30 12:24:23 +0000

    Step 12: Starting with Two Columns

--- webapp/Component.js
+++ webapp/Component.js
@@ -35,8 +35,9 @@ sap.ui.define([
             return this._getFcl().then(function(oFCL) {
                 var oSettings = {
 					defaultTwoColumnLayoutType: fioriLibrary.LayoutType.TwoColumnsMidExpanded,
-					defaultThreeColumnLayoutType: fioriLibrary.LayoutType.ThreeColumnsMidExpanded
-				};
+					defaultThreeColumnLayoutType: fioriLibrary.LayoutType.ThreeColumnsMidExpanded,
+                    initialColumnsCount: 2
+                };
 				return (FlexibleColumnLayoutSemanticHelper.getInstanceFor(oFCL, oSettings));
             });
 
