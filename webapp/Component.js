2a2a9d2803ac7a9b5a7f7a3f4103f2e3b6845eb1
Hasan Tugay Birihan <hasantugay.birihan@nttdata.com>
2022-06-30 09:48:49 +0000

    Step 7: Routing

--- webapp/Component.js
+++ webapp/Component.js
@@ -1,7 +1,8 @@
 sap.ui.define([
 	'sap/ui/core/UIComponent',
-    'sap/ui/model/json/JSONModel'
-], function(UIComponent, JSONModel) {
+    'sap/ui/model/json/JSONModel',
+    'sap/f/library'
+], function(UIComponent, JSONModel, fioriLibrary) {
 	'use strict';
 
 	return UIComponent.extend('sap.ui.demo.fiori2.Component', {
@@ -10,14 +11,32 @@ sap.ui.define([
 			manifest: 'json'
 		},
         init: function () {
-			var oProductsModel;
+			var oModel,
+                oProductsModel, 
+                oRouter; 
 
 			UIComponent.prototype.init.apply(this, arguments);
+
+            oModel = new JSONModel();
+            this.setModel(oModel);
 
 			// set products demo model on this sample
 			oProductsModel = new JSONModel(sap.ui.require.toUrl('sap/ui/demo/mock') + '/products.json');
 			oProductsModel.setSizeLimit(1000);
 			this.setModel(oProductsModel, 'products');
-		}
+
+            oRouter = this.getRouter(),
+            oRouter.attachBeforeRouteMatched(this._onBeforeRouteMatched, this);
+            oRouter.initialize();
+		},
+        _onBeforeRouteMatched: function (oEvent) {
+            var oModel  = this.getModel(),
+                sLayout = oEvent.getParameters().arguments.layout;
+                
+                if (!sLayout) {
+                    sLayout  = fioriLibrary.LayoutType.OneColumn;
+                }
+                oModel.setProperty("/layout", sLayout);
+        }
 	});
 });
