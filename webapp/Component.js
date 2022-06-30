9404149e8106bae5ca3a6f322941b573e8782861
Hasan Tugay Birihan <hasantugay.birihan@nttdata.com>
2022-06-30 12:20:56 +0000

    Step 11: Using the Flexible Column Layout Semantic Helper

--- webapp/Component.js
+++ webapp/Component.js
@@ -1,8 +1,9 @@
 sap.ui.define([
 	'sap/ui/core/UIComponent',
     'sap/ui/model/json/JSONModel',
+    'sap/f/FlexibleColumnLayoutSemanticHelper',
     'sap/f/library'
-], function(UIComponent, JSONModel, fioriLibrary) {
+], function(UIComponent, JSONModel, FlexibleColumnLayoutSemanticHelper, fioriLibrary) {
 	'use strict';
 
 	return UIComponent.extend('sap.ui.demo.fiori2.Component', {
@@ -29,14 +30,45 @@ sap.ui.define([
             oRouter.attachBeforeRouteMatched(this._onBeforeRouteMatched, this);
             oRouter.initialize();
 		},
+
+        getHelper: function () {
+            return this._getFcl().then(function(oFCL) {
+                var oSettings = {
+					defaultTwoColumnLayoutType: fioriLibrary.LayoutType.TwoColumnsMidExpanded,
+					defaultThreeColumnLayoutType: fioriLibrary.LayoutType.ThreeColumnsMidExpanded
+				};
+				return (FlexibleColumnLayoutSemanticHelper.getInstanceFor(oFCL, oSettings));
+            });
+
+         },
+
+
         _onBeforeRouteMatched: function (oEvent) {
             var oModel  = this.getModel(),
-                sLayout = oEvent.getParameters().arguments.layout;
+                sLayout = oEvent.getParameters().arguments.layout,
+                oNextUIState;
                 
                 if (!sLayout) {
-                    sLayout  = fioriLibrary.LayoutType.OneColumn;
+                    this.getHelper().then(function(oHelper) {
+                        oNextUIState = oHelper.getNextUIState(0);
+                        oModel.setProperty("/layout", oNextUIState.layout);
+                    });
+                    return;
                 }
                 oModel.setProperty("/layout", sLayout);
-        }
+        },
+		_getFcl: function () {
+			return new Promise(function(resolve, reject) {
+				var oFCL = this.getRootControl().byId('flexibleColumnLayout');
+				if (!oFCL) {
+					this.getRootControl().attachAfterInit(function(oEvent) {
+						resolve(oEvent.getSource().byId('flexibleColumnLayout'));
+					}, this);
+					return;
+				}
+				resolve(oFCL);
+
+			}.bind(this));
+		}        
 	});
 });
--- webapp/controller/Detail.controller.js
+++ webapp/controller/Detail.controller.js
@@ -1,15 +1,14 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller",
-	'sap/f/library'
-], function (Controller, fioriLibrary) {
+	"sap/ui/core/mvc/Controller"
+], function (Controller) {
 	"use strict";
 
 	return Controller.extend("sap.ui.demo.fiori2.controller.Detail", {
 		onInit: function () {
-			var oOwnerComponent = this.getOwnerComponent();
+			this.oOwnerComponent = this.getOwnerComponent();
 
-			this.oRouter = oOwnerComponent.getRouter();
-			this.oModel = oOwnerComponent.getModel();
+			this.oRouter = this.oOwnerComponent.getRouter();
+			this.oModel = this.oOwnerComponent.getModel();
 
 			this.oRouter.getRoute("master").attachPatternMatched(this._onProductMatched, this);
 			this.oRouter.getRoute("detail").attachPatternMatched(this._onProductMatched, this);
@@ -18,9 +17,17 @@ sap.ui.define([
 
 		onSupplierPress: function (oEvent) {
 			var supplierPath = oEvent.getSource().getBindingContext("products").getPath(),
-				supplier = supplierPath.split("/").slice(-1).pop();
+				supplier = supplierPath.split("/").slice(-1).pop(),
+				oNextUIState;
 
-			this.oRouter.navTo("detailDetail", {layout: fioriLibrary.LayoutType.ThreeColumnsMidExpanded, supplier: supplier, product: this._product});
+			this.oOwnerComponent.getHelper().then(function (oHelper) {
+				oNextUIState = oHelper.getNextUIState(2);
+				this.oRouter.navTo("detailDetail", {
+					layout: oNextUIState.layout,
+					supplier: supplier,
+					product: this._product
+				});
+			}.bind(this));
 		},
 
 		_onProductMatched: function (oEvent) {
@@ -36,6 +43,21 @@ sap.ui.define([
 				bCurrentShowFooterState = oObjectPage.getShowFooter();
 
 			oObjectPage.setShowFooter(!bCurrentShowFooterState);
+		},
+
+		handleFullScreen: function () {
+			var sNextLayout = this.oModel.getProperty("/actionButtonsInfo/midColumn/fullScreen");
+			this.oRouter.navTo("detail", {layout: sNextLayout, product: this._product});
+		},
+
+		handleExitFullScreen: function () {
+			var sNextLayout = this.oModel.getProperty("/actionButtonsInfo/midColumn/exitFullScreen");
+			this.oRouter.navTo("detail", {layout: sNextLayout, product: this._product});
+		},
+
+		handleClose: function () {
+			var sNextLayout = this.oModel.getProperty("/actionButtonsInfo/midColumn/closeColumn");
+			this.oRouter.navTo("master", {layout: sNextLayout});
 		},
 
 		onExit: function () {
